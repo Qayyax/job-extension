07ea62bbbf5c9939b9758e171fcb6bc2c672ac56
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-04-14 14:07:13 -0400

    working on linkedin parsing

--- content.js
+++
@@ -1,3 +0,0 @@
-// Here is where I want to parse the data
-// I still need to know what background.js does
-// lets try this with linkedin
---
+++ linkedin.js
@@ -0,0 +1,9 @@
+// Here is where I want to parse the data
+// I still need to know what background.js does
+// lets try this with linkedin
+const jobDescription = document.querySelector(".jobs-description__container");
+console.log(jobDescription.textContent);
+// https://www.linkedin.com/jobs/collections/
+// So I think I would break it down instead, would make for linkedin, make for indeed
+// and leave it open source for future contributions
+//
