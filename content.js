3cae71cc7595bf586bb9d0756be6591645f9ac06
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-04-14 16:49:04 -0400

    fixed interval

--- content.js
+++ content.js
@@ -1,12 +1,30 @@
 console.log("this script is working");
 
-setInterval(() => {
-  const jobDetails = document.querySelector("#job-details");
-  if (jobDetails) {
-    console.log(jobDetails.textContent);
-  }
-  console.log("searching...");
-}, 2000);
+const getData = async () => {
+  return new Promise((resolve, reject) => {
+    let interval;
+    let counter = 0;
+    interval = setInterval(() => {
+      const jobDetails = document.querySelector("#job-details");
+      counter++;
+      if (jobDetails) {
+        clearInterval(interval);
+        resolve(jobDetails.textContent);
+      }
+      console.log("searching...");
+      if (counter > 50) {
+        clearInterval(interval);
+        reject("Timeout....");
+      }
+    }, 1000);
+  });
+};
+
+// getData().then((data) => console.log("FINALLY:", data));
+(async () => {
+  const data = await getData();
+  console.log(data);
+})();
 
 //
 //"https://www.linkedin.com/jobs/collections/*",
