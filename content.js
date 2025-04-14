8de063fedd36229ef825d7489dbab335c1cef665
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-04-14 16:18:49 -0400

    interval fix

--- content.js
+++ content.js
@@ -1,21 +1,14 @@
 console.log("this script is working");
 
-let jobDetail = "";
-const interval = setInterval(() => {
-  const testing = document.querySelector("#job-details");
-  if (testing) {
-    // clearInterval(interval);
-    jobDetail = testing;
+setInterval(() => {
+  const jobDetails = document.querySelector("#job-details");
+  if (jobDetails) {
+    console.log(jobDetails.textContent);
   }
-  console.log("..searching");
+  console.log("searching...");
 }, 2000);
 
-if (jobDetail.length > 10) {
-  clearInterval(interval);
-}
-
-console.log(jobDetail);
-
+//
 //"https://www.linkedin.com/jobs/collections/*",
 // so it was  a react problem
 // would make a function to run every interval until "testing exist, when testing does, then we continue"
